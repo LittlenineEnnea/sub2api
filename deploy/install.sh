5cc1ca7ee67495f20a8ca5bbfd69bdc222f90279
Littlenine <LittlenineEnnea@gmail.com>
2026-07-28 23:31:31 +0800

    Update install.sh

--- deploy/install.sh
+++ deploy/install.sh
@@ -39,7 +39,7 @@ CONFIG_DIR="/etc/sub2api"
 
 # Server configuration (will be set by user)
 SERVER_HOST="0.0.0.0"
-SERVER_PORT="8080"
+SERVER_PORT="8081"
 
 # Language (default: zh = Chinese)
 LANG_CHOICE="zh"
